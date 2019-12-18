df648c8e945a02c71fe2ae85b65df75054f3a17c
MekalaParameswaraReddy <parameswarareddy2@gmail.com>
2019-12-18 09:00:43 +0530

    Collection mapping: queries are added.

--- HibernateApps/src/com/paramesh/mapping/collection/CollecctionMapping.sql
+++ HibernateApps/src/com/paramesh/mapping/collection/CollecctionMapping.sql
@@ -5,3 +5,22 @@ memid int(10),
 indexs varchar(50),
 plans varchar(50),
 FOREIGN KEY (memid) REFERENCES  member(memberId))
+
+-- Bag Example
+create table benifitPlan (
+memid int(10),
+plans varchar(50),
+FOREIGN KEY (memid) REFERENCES  member(memberId))
+
+-- set Example
+create table uniqueBenifitPlan (
+memid int(10),
+plans varchar(50),
+FOREIGN KEY (memid) REFERENCES  member(memberId))
+
+-- Map example
+create table planNameAndExplanation (
+memberID int(10),
+plan varchar(50),
+Description varchar(50),
+FOREIGN KEY (memid) REFERENCES  member(memberId))
